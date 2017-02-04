675a8d6ee839e1323f394e075a7e8fa9fe66aa72
maximedegreve <maximedegreve@me.com>
2017-02-04 18:10:35 +0000

    WIP to support symbols

--- TinyFaces.sketchplugin/Contents/Sketch/fill.js
+++ TinyFaces.sketchplugin/Contents/Sketch/fill.js
@@ -6,75 +6,127 @@ function onRun(context) {
   var command = context.command;
   var identifier = [command identifier];
 
- if (selection.length > 0) {
+  if (selection.length == 0) {
+    [doc showMessage:"No layer is selected."];
+    return;
+  }
 
-NSLog("identifier : " + identifier)
+  var minQuality = 0
+  var gender = ""
 
-   var minQuality = 0
-   var gender = ""
+  if (identifier == "fillhigh"){
+    minQuality = 6
+  }
 
-   if (identifier == "fillhigh"){
-     minQuality = 6
-   }
+  if (identifier == "fillmale"){
+    gender = "&gender=male"
+  }
 
-   if (identifier == "fillmale"){
-     gender = "&gender=male"
-   }
+  if (identifier == "fillfemale"){
+    gender = "&gender=female"
+  }
 
-   if (identifier == "fillfemale"){
-     gender = "&gender=female"
-   }
+  var request = [[NSMutableURLRequest alloc] init];
+  [request setHTTPMethod:@"GET"];
+  var queryString = "https://tinyfac.es/api/users/?min_quality=" + minQuality + gender;
+  [request setURL:[NSURL URLWithString:queryString]];
 
-   var request = [[NSMutableURLRequest alloc] init];
-   [request setHTTPMethod:@"GET"];
-   var queryString = "https://tinyfac.es/api/users/?min_quality=" + minQuality + gender;
-   NSLog(queryString)
-   [request setURL:[NSURL URLWithString:queryString]];
+  var error = [[NSError alloc] init];
+  var responseCode = null;
 
-   var error = [[NSError alloc] init];
-   var responseCode = null;
+  var oResponseData = [NSURLConnection sendSynchronousRequest:request returningResponse:responseCode error:error];
 
-   var oResponseData = [NSURLConnection sendSynchronousRequest:request returningResponse:responseCode error:error];
+  var dataString = [[NSString alloc] initWithData:oResponseData
+  encoding:NSUTF8StringEncoding];
 
-   var dataString = [[NSString alloc] initWithData:oResponseData
-   encoding:NSUTF8StringEncoding];
+  var pattern = new RegExp("\\\\'", "g");
+  var validJSONString = dataString.replace(pattern, "'");
 
-   var pattern = new RegExp("\\\\'", "g");
-   var validJSONString = dataString.replace(pattern, "'");
+  var data = JSON.parse(validJSONString);
 
-   var data = JSON.parse(validJSONString);
+  if (data.length == 0) {
+    var message = "Something went wrong getting data from tinyfac.es. Try again later?";
+    [doc showMessage: message];
+    return;
+  }
 
-   if (data.length > 0) {
-     for (var i=0; i <= selection.length; i++) {
+  var imagesArray = [];
+  var namesArray = [];
 
-      var layer = [selection objectAtIndex:i];
+  data.forEach(function(item){
+    var imageURL = item.avatars[2].url;
+    imagesArray.push(imageURL);
+    var name = item.first_name + " " + item.last_name;
+    namesArray.push(name);
+  });
 
-      if (layer.className() == "MSTextLayer")){
+  selection.forEach(function(layer){
+    fillLayer(layer, imagesArray, namesArray);
+  });
 
-              layer.stringValue = data[i].first_name + " " + data[i].last_name;
+}
+
+function generateImageData(url){
+  var url = [[NSURL alloc] initWithString: url];
+  var newImage = [[NSImage alloc] initByReferencingURL:url];
+  return MSImageData.alloc().initWithImage_convertColorSpace(newImage, false)
+}
+
+function getFirstAndRemoveFromArray(array){
+  var value = array.splice(0,1)[0];
+  return value
+}
+
+function fillLayer(layer, imagesArray, namesArray){
+
+  if (layer.className() == "MSTextLayer")){
+
+    var name = getFirstAndRemoveFromArray(namesArray)
+    layer.stringValue = names;
+
+  } else if (layer.className() == "MSSymbolInstance"){
+
+    //let layersInSymbol = layer.symbolMaster().layers()
+    //log("count")
+    //log(layersInSymbol.length);
+    /*
+    for (var i=0; i <= layersInSymbol.length; i++) {
+
+      if (layer.className() == "MSShapeGroup")){
+
+
 
       } else if (layer.className() == "MSShapeGroup")){
 
-              var imageURLString = data[i].avatars[2].url;
-               var url = [[NSURL alloc] initWithString: imageURLString];
+        // turn the overrides mutable
+        //var mutableOverrides = NSMutableDictionary.dictionaryWithDictionary(existingOverrides);
 
-               var newImage = [[NSImage alloc] initByReferencingURL:url];
+        // get the first one
+        //var firstOverride = NSMutableDictionary.dictionaryWithDictionary(existingOverrides.objectForKey(0));
 
-               var fill = layer.style().fills().firstObject();
-               fill.setFillType(4);
-               fill.setImage(MSImageData.alloc().initWithImage_convertColorSpace(newImage, false));
-               fill.setPatternFillType(1);
+        // set the first override to the imageData
+        //var overrideId = [firstOverride allKeys][0];
+        //firstOverride.setObject_forKey(imageData,overrideId)
+
       }
 
+    }
+    */
 
+  } else if (layer.className() == "MSShapeGroup")){
 
-     }
-   } else {
-     var message = "No images found tagged with: " + tag;
-     [doc showMessage: message];
-   }
- } else {
-   [doc showMessage:"No layer is selected."];
- }
+      var imageURLString = getFirstAndRemoveFromArray(imagesArray)
+      var fill = layer.style().fills().firstObject();
+      fill.setFillType(4);
+      fill.setImage(generateImageData(imageURLString));
+      fill.setPatternFillType(1);
+
+  } else if (layer.className() == "MSLayerGroup")){
+
+    layer.layers().forEach(function(layer){
+      fillLayer(layer, imagesArray, namesArray)
+    });
+
+  }
 
 }
